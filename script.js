f5de4468cc067e1857e9bd1d7d7121086534f87a
nathaliafreboucas <contato.nathaliareboucas@outlook.com>
2022-04-28 00:18:58 -0300

    commit para adicionar script em um arquivo separado

---
+++ script.js
@@ -0,0 +1,62 @@
+ 
+var timestamp = new Date();
+document.getElementById('info').innerHTML = timestamp.getFullYear();
+
+function criptografarString(){
+    var procura = /[A-Z&&áàâãä-úùũûü&&Çç]/;
+    var procuraLetra = texto.value.search(procura); 
+        
+    if(procuraLetra != -1){
+        alert ('Permitido apenas letras minúsculas e sem acentuação.Tente novamente!');
+    }else{
+        textoCriptografado = texto.value;
+        textoCriptografado = textoCriptografado.replaceAll('e', 'enter').replaceAll('i', 'imes')
+        .replaceAll('a', 'ai').replaceAll('o', 'ober').replaceAll('u', 'ufat');
+        
+        document.getElementById('mostra-mensagem').innerHTML = textoCriptografado;
+        texto.value = ""; 
+    } 
+        
+}
+            
+function decriptografarString(){
+    var procura = /[A-Z&&áàâãä-úùũûü&&Çç]/;
+    var procuraLetra = texto.value.search(procura); 
+        
+    if(procuraLetra != -1){
+        alert ('Permitido apenas letras minúsculas e sem acentuação.Tente novamente!');
+    }else{
+        
+        textoDecriptografado = texto.value;
+        textoDecriptografado = textoDecriptografado.replaceAll('enter', 'e').replaceAll('imes', 'i')
+        .replaceAll('ai', 'a').replaceAll('ober', 'o').replaceAll('ufat', 'u');
+        
+        document.getElementById('mostra-mensagem').innerHTML = textoDecriptografado;
+        texto.value = "";
+    }    
+                    
+} 
+        
+function copiar(){
+        
+    var copia = document.getElementById('mostra-mensagem');
+    var range = document.createRange();
+        
+    range.selectNode(copia);
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    alert('Copiado para a área de transferencia!')
+        
+}   
+        
+var textoCriptografado;
+var textoDecriptografado;
+var texto = document.querySelector('textarea');
+var botaoCopiar = document.getElementById('copy');
+            
+var botaoCriptografa = document.getElementById('encrypt');
+var botaoDecriptografa = document.getElementById('decrypt');
+            
+botaoCriptografa.onclick = criptografarString;
+botaoDecriptografa.onclick = decriptografarString;
+botaoCopiar.onclick = copiar;
